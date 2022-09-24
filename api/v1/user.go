c348bef4b3bd8b6c4753405681186ca031b9f3ca
1mjtrack <1174511382@qq.com>
2022-09-24 14:58:02 +0800

    GinBlogv1.1

--- api/v1/user.go
+++ api/v1/user.go
@@ -12,7 +12,7 @@ import (
 
 var code int
 
-// 添加用户
+// AddUser 添加用户
 func AddUser(c *gin.Context) {
 	var data model.User
 	var msg string
@@ -44,7 +44,7 @@ func AddUser(c *gin.Context) {
 
 //查询单个用户
 
-// 查询用户列表
+// GetUsers 查询用户列表
 func GetUsers(c *gin.Context) {
 	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
 	pageNumber, _ := strconv.Atoi(c.Query("pagenumber"))
@@ -64,7 +64,7 @@ func GetUsers(c *gin.Context) {
 	})
 }
 
-// 编辑用户
+// EditUser 编辑用户
 func EditUser(c *gin.Context) {
 	var data model.User
 	id, _ := strconv.Atoi(c.Param("id"))
@@ -83,7 +83,7 @@ func EditUser(c *gin.Context) {
 	})
 }
 
-// 删除用户
+// DeleteUser 删除用户
 func DeleteUser(c *gin.Context) {
 	id, _ := strconv.Atoi(c.Param("id"))
 
